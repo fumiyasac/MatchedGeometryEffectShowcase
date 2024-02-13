7e22ab2a870239e0dc4d76f839e34deec096c2f9
fumiyasac <just1factory@gmail.com>
2024-02-13 17:55:05 +0900

    initial commit

---
+++ MatchedGeometryEffectShowcase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MatchedGeometryEffectShowcase
+//
+//  Created by 酒井文也 on 2024/02/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MatchedGeometryEffectShowcase/MatchedGeometryEffectShowcaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchedGeometryEffectShowcaseApp.swift
+//  MatchedGeometryEffectShowcase
+//
+//  Created by 酒井文也 on 2024/02/13.
+//
+
+import SwiftUI
+
+@main
+struct MatchedGeometryEffectShowcaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
