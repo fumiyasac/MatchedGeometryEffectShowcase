c142635b77728765d9f18a9e5c686321cc67f4f7
fumiyasac <just1factory@gmail.com>
2024-04-13 19:01:26 +0900

    比較用に準備するUIKit用サンプルの事前準備をする

---
+++ iOSApp/MatchedGeometryEffectShowcase/Application/Screen/PopularScreenView/PopularScreenViewComponent/PopularDetailHeaderView/PopularDetailHeaderView.swift
@@ -0,0 +1,10 @@
+//
+//  PopularDetailHeaderView.swift
+//  MatchedGeometryEffectShowcase
+//
+//  Created by 酒井文也 on 2024/04/13.
+//
+
+import UIKit
+
+final class PopularDetailHeaderView: UIView {}
---
+++ iOSApp/MatchedGeometryEffectShowcase/Application/ViewStateProvider/PopularViewStateProvider.swift
@@ -0,0 +1,11 @@
+//
+//  PopularViewStateProvider.swift
+//  MatchedGeometryEffectShowcase
+//
+//  Created by 酒井文也 on 2024/04/13.
+//
+
+import Observation
+
+@Observable
+public final class PopularViewStateProvider {}
