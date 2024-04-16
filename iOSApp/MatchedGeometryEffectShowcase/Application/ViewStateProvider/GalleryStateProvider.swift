fb97084e979f7c89347de27fc8bb3eaf860d9b53
fumiyasac <just1factory@gmail.com>
2024-04-16 23:35:46 +0900

    GalleryStateProviderを新規追加

---
+++ iOSApp/MatchedGeometryEffectShowcase/Application/ViewStateProvider/GalleryStateProvider.swift
@@ -0,0 +1,11 @@
+//
+//  GalleryStateProvider.swift
+//  MatchedGeometryEffectShowcase
+//
+//  Created by 酒井文也 on 2024/04/16.
+//
+
+import Foundation
+
+@Observable
+public final class GalleryStateProvider {}
