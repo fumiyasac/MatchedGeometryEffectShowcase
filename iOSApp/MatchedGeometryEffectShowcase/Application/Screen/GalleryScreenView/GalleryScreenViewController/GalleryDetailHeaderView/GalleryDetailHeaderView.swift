bcfa4d8f9d83c16b2afcfb5ec070daeeaac10f96
fumiyasac <just1factory@gmail.com>
2024-04-17 23:53:35 +0900

    GalleryDetailHeaderView.swiftの新規追加

---
+++ iOSApp/MatchedGeometryEffectShowcase/Application/Screen/GalleryScreenView/GalleryScreenViewController/GalleryDetailHeaderView/GalleryDetailHeaderView.swift
@@ -0,0 +1,24 @@
+//
+//  GalleryDetailHeaderView.swift
+//  MatchedGeometryEffectShowcase
+//
+//  Created by 酒井文也 on 2024/04/17.
+//
+
+import Foundation
+import UIKit
+
+final class GalleryDetailHeaderView: CustomViewBase {
+
+    // MARK: - Property
+
+    // MARK: - Initializer
+
+    required init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
