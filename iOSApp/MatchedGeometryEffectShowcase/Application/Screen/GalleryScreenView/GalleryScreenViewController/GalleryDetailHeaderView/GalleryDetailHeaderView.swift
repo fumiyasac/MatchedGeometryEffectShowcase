be355745cfff98fbc5937c15d238cc3522bd8d57
fumiyasac <just1factory@gmail.com>
2024-04-18 09:25:15 +0900

    GalleryDetailHeaderView.swiftの実装を準備

--- iOSApp/MatchedGeometryEffectShowcase/Application/Screen/GalleryScreenView/GalleryScreenViewController/GalleryDetailHeaderView/GalleryDetailHeaderView.swift
+++ iOSApp/MatchedGeometryEffectShowcase/Application/Screen/GalleryScreenView/GalleryScreenViewController/GalleryDetailHeaderView/GalleryDetailHeaderView.swift
@@ -12,13 +12,49 @@ final class GalleryDetailHeaderView: CustomViewBase {
 
     // MARK: - Property
 
+    var headerBackButtonTappedHandler: (() -> ())?
+
+    @IBOutlet weak private var headerBackButton: UIButton!
+    @IBOutlet weak private var headerTitleLabel: UILabel!
+
     // MARK: - Initializer
 
     required init(frame: CGRect) {
         super.init(frame: frame)
+
+        setupGalleryDetailHeaderView()
     }
 
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+
+        setupGalleryDetailHeaderView()
+    }
+
+    // MARK: - Function
+
+    func setTitle(_ title: String) {
+        headerTitleLabel.text = title
+    }
+
+    func changeAlpha(_ targetAlpha: CGFloat) {
+        if targetAlpha > 1 {
+            headerTitleLabel.alpha = 1
+        } else if 0...1 ~= targetAlpha {
+            headerTitleLabel.alpha = targetAlpha
+        } else {
+            headerTitleLabel.alpha = 0
+        }
+    }
+
+    // MARK: - Private Function
+
+    private func setupGalleryDetailHeaderView() {
+        headerBackButton.addTarget(self, action:  #selector(self.headerBackButtonTapped(sender:)), for: .touchUpInside)
+    }
+
+    // ViewController側でクロージャー内にセットした処理を実行する
+    @objc private func headerBackButtonTapped(sender: UIButton) {
+        headerBackButtonTappedHandler?()
     }
 }
