15e09bff2a79827167bb022cef8a21da991e9dfc
fumiyasac <just1factory@gmail.com>
2024-04-21 16:26:08 +0900

    ViewObjectの雛形を決定する

---
+++ iOSApp/MatchedGeometryEffectShowcase/Application/ViewObject/PickupViewObject.swift
@@ -0,0 +1,12 @@
+//
+//  PickupViewObject.swift
+//  MatchedGeometryEffectShowcase
+//
+//  Created by 酒井文也 on 2024/04/21.
+//
+
+import Foundation
+
+// TODO: 表示内容の構造体を決定する
+
+struct PickupViewObject {}
