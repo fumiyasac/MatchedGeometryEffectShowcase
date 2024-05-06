d40d2327f268d77256e950645a3a5ed026e56540
fumiyasac <just1factory@gmail.com>
2024-05-07 07:36:01 +0900

    FeedViewStateProviderを新規追加する

---
+++ iOSApp/MatchedGeometryEffectShowcase/Application/ViewStateProvider/FeedViewStateProvider.swift
@@ -0,0 +1,11 @@
+//
+//  FeedViewStateProvider.swift
+//  MatchedGeometryEffectShowcase
+//
+//  Created by 酒井文也 on 2024/05/07.
+//
+
+import Foundation
+
+@Observable
+public final class FeedViewStateProvider {}
