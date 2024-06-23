ca7442ff0eff8c82a2f3207db022f3fd9098e83a
fumiyasac <just1factory@gmail.com>
2024-06-23 22:10:39 +0900

    表示用Entityの新規追加

---
+++ iOSApp/MatchedGeometryEffectShowcase/Application/Entity/ProductEntity.swift
@@ -0,0 +1,10 @@
+//
+//  ProductEntity.swift
+//  MatchedGeometryEffectShowcase
+//
+//  Created by 酒井文也 on 2024/06/23.
+//
+
+import Foundation
+
+struct ProductEntity {}
---
+++ iOSApp/MatchedGeometryEffectShowcase/Application/Entity/UserPhotoEntity.swift
@@ -0,0 +1,10 @@
+//
+//  UserPhotoEntity.swift
+//  MatchedGeometryEffectShowcase
+//
+//  Created by 酒井文也 on 2024/06/23.
+//
+
+import Foundation
+
+struct UserPhotoEntity {}
