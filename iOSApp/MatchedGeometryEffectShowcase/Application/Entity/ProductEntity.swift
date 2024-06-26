83dd3e3e39f8269bb28b1372c5f1f1f841acc1d8
fumiyasac <just1factory@gmail.com>
2024-06-26 21:27:33 +0900

    ProductEntityの新規作成

--- iOSApp/MatchedGeometryEffectShowcase/Application/Entity/ProductEntity.swift
+++ iOSApp/MatchedGeometryEffectShowcase/Application/Entity/ProductEntity.swift
@@ -7,4 +7,21 @@
 
 import Foundation
 
-struct ProductEntity {}
+struct ProductEntity {
+
+    // MARK: - Property
+
+    let identifier: UUID = UUID()
+    let productID: Int
+    let isPremium: Bool
+    let isSale: Bool
+    let percentSale: Int
+    let regularPrice: Int
+    let productName: String
+    let productSummary: String
+    let productDescription: String
+    let mainCategory: String
+    let subCategory: String
+    let hashtags: [String]
+    let productPhotoUrl: URL?
+}
