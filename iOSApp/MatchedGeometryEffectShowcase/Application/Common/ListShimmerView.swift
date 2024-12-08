53a7e66e0fbd7963a81e308f573957b58a03e628
fumiyasac <just1factory@gmail.com>
2024-12-08 20:29:27 +0900

    ListShimmerView新規追加

---
+++ iOSApp/MatchedGeometryEffectShowcase/Application/Common/ListShimmerView.swift
@@ -0,0 +1,18 @@
+//
+//  ListShimmerView.swift
+//  MatchedGeometryEffectShowcase
+//
+//  Created by 酒井文也 on 2024/12/08.
+//
+
+import SwiftUI
+
+struct ListShimmerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListShimmerView()
+}
