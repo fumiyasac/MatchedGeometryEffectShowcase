ed340620296159d1ab5311224f4bb36c9853b492
fumiyasac <just1factory@gmail.com>
2024-12-10 15:43:22 +0900

    GalleryShimmerViewを新規作成

---
+++ iOSApp/MatchedGeometryEffectShowcase/Application/Common/GalleryShimmerView.swift
@@ -0,0 +1,33 @@
+//
+//  GalleryShimmerView.swift
+//  MatchedGeometryEffectShowcase
+//
+//  Created by 酒井文也 on 2024/12/10.
+//
+
+import SwiftUI
+
+// TODO: Color/Fontはデザインは後程決める
+
+struct GalleryShimmerView: View {
+
+    // MARK: - Typealias
+
+    // MARK: - Property
+
+    // MARK: - Initializer
+
+    init() {}
+
+    // MARK: - Body
+
+    var body: some View {
+        Text("GalleryShimmerView")
+    }
+}
+
+// MARK: - Preview
+
+#Preview {
+    GalleryShimmerView()
+}
