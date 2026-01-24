cb6a168f2fa78afc63ab51d178b41db95077283b
fumiyasac <just1factory@gmail.com>
2026-01-24 16:03:53 +0900

    PopularScreenDetailViewControlleクラスの新規追加r

---
+++ iOSApp/MatchedGeometryEffectShowcase/Application/Screen/PopularScreenView/PopularScreenViewController/PopularScreenDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PopularScreenDetailViewController.swift
+//  MatchedGeometryEffectShowcase
+//
+//  Created by 酒井文也 on 2026/01/24.
+//
+
+import UIKit
+
+final class PopularScreenDetailViewController: UIViewController {
+
+    // MARK: - Override
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
